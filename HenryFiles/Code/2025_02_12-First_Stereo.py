6416f48b4e908138c77f07d2a2bc484093068741
Henry Howe <heh44@aber.ac.uk>
2025-02-12 16:16:07 +0000

    added starting test with stereo

---
+++ HenryFiles/Code/2025_02_12-First_Stereo.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+import numpy as np
+
+left_img = cv.imread('HenryFiles\AUPE Images\distance\pctset-1m-8bit\distance_pctset-1m-8bit_LWAC01_T00_P00_BS.png')
+right_img = cv.imread('HenryFiles\AUPE Images\distance\pctset-1m-8bit\distance_pctset-1m-8bit_RWAC01_T00_P00_BS.png')
+
+orb = cv.ORB_create()
+kp1, des1 = orb.detectAndCompute(left_img, None)
+kp2, des2 = orb.detectAndCompute(right_img, None)
+
+bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+matches = bf.match(des1, des2)
+matches = sorted(matches, key=lambda x: x.distance)
+
+result = cv.drawMatches(left_img, kp1, right_img, kp2, matches[:10], None, flags=2)
+
+cv.imshow('result', result)
+cv.waitKey(0)
+cv.destroyAllWindows()

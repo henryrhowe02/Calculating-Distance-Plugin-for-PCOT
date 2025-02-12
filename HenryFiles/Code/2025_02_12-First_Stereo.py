654fb350ee87b699bf227bc8a9d433b99feaa0c7
Henry Howe <heh44@aber.ac.uk>
2025-02-12 16:23:17 +0000

    updated with working window, code now finds similar points between two images

--- HenryFiles/Code/2025_02_12-First_Stereo.py
+++ HenryFiles/Code/2025_02_12-First_Stereo.py
@@ -4,6 +4,10 @@ import numpy as np
 left_img = cv.imread('HenryFiles\AUPE Images\distance\pctset-1m-8bit\distance_pctset-1m-8bit_LWAC01_T00_P00_BS.png')
 right_img = cv.imread('HenryFiles\AUPE Images\distance\pctset-1m-8bit\distance_pctset-1m-8bit_RWAC01_T00_P00_BS.png')
 
+if left_img is None or right_img is None:
+    print("Image not found")
+    exit()
+
 orb = cv.ORB_create()
 kp1, des1 = orb.detectAndCompute(left_img, None)
 kp2, des2 = orb.detectAndCompute(right_img, None)
@@ -14,6 +18,11 @@ matches = sorted(matches, key=lambda x: x.distance)
 
 result = cv.drawMatches(left_img, kp1, right_img, kp2, matches[:10], None, flags=2)
 
+# cv.imshow('result', result)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+cv.namedWindow('result', cv.WINDOW_NORMAL)
 cv.imshow('result', result)
 cv.waitKey(0)
 cv.destroyAllWindows()

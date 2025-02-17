fcab4215511f6a34cd5cfef816f1158a60052090
Henry Howe <heh44@aber.ac.uk>
2025-02-17 13:26:58 +0000

    adjusted orb to capture more features, adjusted scale factor to 1.2 (decrease from default 1.5

--- HenryFiles/Code/2025_02_12-First_Stereo.py
+++ HenryFiles/Code/2025_02_12-First_Stereo.py
@@ -8,7 +8,8 @@ if left_img is None or right_img is None:
     print("Image not found")
     exit()
 
-orb = cv.ORB_create()
+# orb = cv.ORB_create()
+orb = cv.ORB_create(nfeatures=1000, scaleFactor=1.2, nlevels=8, edgeThreshold=31, firstLevel=0, WTA_K=2, patchSize=31, fastThreshold=20)
 kp1, des1 = orb.detectAndCompute(left_img, None)
 kp2, des2 = orb.detectAndCompute(right_img, None)
 
@@ -16,7 +17,10 @@ bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
 matches = bf.match(des1, des2)
 matches = sorted(matches, key=lambda x: x.distance)
 
-result = cv.drawMatches(left_img, kp1, right_img, kp2, matches[:10], None, flags=2)
+result = cv.drawMatches(left_img, kp1, right_img, kp2, matches[:50], None, flags=2)
+
+# cv.drawKeypoints(left_img, kp1, left_img, color=(0, 255, 0), flags=cv.DrawMatchesFlags_DRAW_RICH_KEYPOINTS)
+# cv.drawKeypoints(right_img, kp2, right_img, color=(0, 255, 0), flags=cv.DrawMatchesFlags_DRAW_RICH_KEYPOINTS)
 
 # cv.imshow('result', result)
 # cv.waitKey(0)

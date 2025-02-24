0bc9787cffdffc4a1442fdd592fb516a54aee1ae
Henry Howe <heh44@aber.ac.uk>
2025-02-24 14:33:52 +0000

    adjusted opencv camera calibration tutorial

--- HenryFiles/Camera Calibration/Tutorial/Tutorial images/opencv_calibrate_camera.py
+++ HenryFiles/Camera Calibration/Tutorial/Tutorial images/opencv_calibrate_camera.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
 import glob
+import os
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
@@ -9,7 +10,10 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
-images = glob.glob('*.jpg')
+
+# images = glob.glob('*.jpg')
+images = glob.glob(os.path.join('C:/Users/henry/pcot-exomars-pancam-major-project/HenryFiles/Camera Calibration/Tutorial/Tutorial images', '*.jpg'))
+print(len(images))
 camera_mats = []
 for fname in images:
 	img = cv.imread(fname)

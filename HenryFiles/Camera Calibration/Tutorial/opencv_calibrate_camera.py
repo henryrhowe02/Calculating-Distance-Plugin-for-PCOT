30273ed6a705bfe264b19b68bf9e2c1130b83c52
Henry Howe <heh44@aber.ac.uk>
2025-02-26 11:32:35 +0000

    minor changes to tutorial

--- HenryFiles/Camera Calibration/Tutorial/opencv_calibrate_camera.py
+++ HenryFiles/Camera Calibration/Tutorial/opencv_calibrate_camera.py
@@ -3,6 +3,12 @@ import cv2 as cv
 import glob
 import os
 
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+
 def calibration(images):
 	# Arrays to store object points and image points from all the images.
 	objpoints = [] # 3d point in real world space
@@ -12,9 +18,18 @@ def calibration(images):
 	camera_mats = []
 	for fname in images:
 		img = cv.imread(fname)
+		if img is None:
+			print(f"Image {fname} not found")
+			continue
+		# Convert to grayscale
 		gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+  
 		# Find the chess board corners
-		ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+		ret, corners = cv.findChessboardCorners(gray, (9,6), None)
+  
+		# # Try different flags in cv.findChessboardCorners
+		# ret, corners = cv.findChessboardCorners(gray, (7, 6), cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_NORMALIZE_IMAGE + cv.CALIB_CB_FAST_CHECK)  
+		print(f"Chessboard detection for {fname}: {ret}")
 		# If found, add object points, image points (after refining them)
 		if ret == True:
 			objpoints.append(objp)
@@ -35,6 +50,7 @@ def calibration(images):
 			cv.waitKey(500)
 	cv.destroyAllWindows()
 	print("====== Camera Matrices ======")
+	print(len(camera_mats))
 	print(np.mean(np.array(camera_mats), axis=0))
  
 tut_images = glob.glob(os.path.join('HenryFiles/Camera Calibration/Tutorial/Tutorial images', '*.jpg'))
@@ -46,15 +62,10 @@ print(len(left_images))
 right_images = glob.glob(os.path.join('HenryFiles/Camera Calibration/right images', '*.png'))
 print(len(right_images))
 
-# termination criteria
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 
 
 # calibration(tut_images)
 
 calibration(left_images)
 
-calibration(right_images)
+# calibration(right_images)

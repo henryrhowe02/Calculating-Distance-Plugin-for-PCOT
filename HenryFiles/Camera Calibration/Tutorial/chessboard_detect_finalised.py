55fa485f2997fe7af335e9bae78a21e4810ef099
Henry Howe <heh44@aber.ac.uk>
2025-03-04 17:17:27 +0000

    adding in thresholding to the final design

--- HenryFiles/Camera Calibration/Tutorial/chessboard_detect_finalised.py
+++ HenryFiles/Camera Calibration/Tutorial/chessboard_detect_finalised.py
@@ -2,7 +2,17 @@ import cv2 as cv
 import numpy as np
 import glob
 
-chessboard_size = (6,6)
+chessboard_size = (7,6)
+
+def adjust_contrast(image, alpha=3, beta=-120):
+    """
+    Adjust the contrast of the image.
+    :param image: input image
+    :param alpha: contrast control (1.0-3.0)
+    :param beta: brightness control (0-100)
+    :return: contrast adjusted image
+    """
+    return cv.convertScaleAbs(image, alpha=alpha, beta=beta)
 
 def find_chessboards(image_files):
     detected_images = []
@@ -11,20 +21,27 @@ def find_chessboards(image_files):
         img = cv.imread(file)
         
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        
-        ret1,gray = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
 
+        # gray = adjust_contrast(gray)
+
+        ret1, gray = cv.threshold(gray, 100, 255, cv.THRESH_BINARY)
+        
+        # ret1,gray = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+
+        cv.imshow('Image before detection', gray)
+        cv.waitKey(0)
 
         # Find the chessboard corners
         # ret, corners = cv.findChessboardCorners(gray, chessboard_size, cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_NORMALIZE_IMAGE + cv.CALIB_CB_FAST_CHECK)
         ret, corners = cv.findChessboardCorners(gray, chessboard_size, None)
+        # ret, corners = cv.findChessboardCorners(gray, chessboard_size, cv.CALIB_CB_ADAPTIVE_THRESH)
 
         if ret:
             detected_images.append(file)
             # Draw and display the corners
             img = cv.drawChessboardCorners(gray, chessboard_size, corners, ret)
             cv.imshow('Chessboard Detection', img)
-            cv.waitKey(0)
+            cv.waitKey(10)
 
     cv.destroyAllWindows()
     return detected_images

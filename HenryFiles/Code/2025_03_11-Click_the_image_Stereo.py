0e02cc19be43b96893292d394cf6a54319bba69a
Henry Howe <heh44@aber.ac.uk>
2025-03-12 13:58:05 +0000

    working on stereo rectification

--- HenryFiles/Code/2025_03_11-Click_the_image_Stereo.py
+++ HenryFiles/Code/2025_03_11-Click_the_image_Stereo.py
@@ -4,6 +4,10 @@ import glob
 import os
 import math
 
+# from Camera_Calibration.Tutorial.opencv_calibrate_camera_function import calibrate_camera
+
+
+
 # Load the images
 imgL_path = r'AUPE Images/distance/pctset-1m-8bit/distance_pctset-1m-8bit_LWAC01_T00_P00_BS.png'
 imgR_path = r'AUPE Images/distance/pctset-1m-8bit/distance_pctset-1m-8bit_RWAC01_T00_P00_BS.png'
@@ -26,11 +30,11 @@ left_matrix = np.array([
     [1.93479347e+03, 0.00000000e+00, 4.94465690e+02], 
     [0.00000000e+00, 1.92439346e+03, 4.16565813e+02], 
     [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]
-    ])
+    ], )
 
 left_dist = np.array([
     [-4.92069166e-02, -8.97949590e-01, -1.33405070e-02, -4.44237271e-03, 2.39096813e+01]
-    ])
+    ], )
 
 f_x_left = left_matrix[0][0]
 f_y_left = left_matrix[1][1]
@@ -41,10 +45,11 @@ right_matrix = np.array([
     [1.93490723e+03, 0.00000000e+00, 5.26855083e+02],
     [0.00000000e+00, 1.92810886e+03, 5.05013225e+02],
     [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]
- ])
+ ], )
 
 right_dist = np.array([
-    [-1.49167371e-01, 1.24309868e+00, -4.14581848e-04, -3.06654869e-03, -7.74292242e+00]])
+    [-1.49167371e-01, 1.24309868e+00, -4.14581848e-04, -3.06654869e-03, -7.74292242e+00]
+    ], )
 
 f_x_right = right_matrix[0][0]
 f_y_right = right_matrix[1][1]
@@ -55,23 +60,81 @@ focal_length_mm = 12
 sensor_width_mm = 8.8
 image_width_pixels = 1024
 
-baseline = 500  # Distance between the two cameras in mm
 
 camera_rotation = 2.8 # Inwards rotation of the cameras in degrees
 total_camera_rotation = camera_rotation * 2
 tcr_radians = math.radians(total_camera_rotation)
+print("tcr_radians: ", tcr_radians)
 
-T = np.array([
-    baseline, 0, 0
-])
-
-R = np.array([
+R_left = np.array([
     [math.cos(tcr_radians), 0, math.sin(tcr_radians)],
     [0, 1, 0],
     [-math.sin(tcr_radians), 0, math.cos(tcr_radians)]
-])
+], dtype=np.float32)
 
+R_right = np.array([
+    [math.cos(-tcr_radians), 0, math.sin(-tcr_radians)],
+    [0, 1, 0],
+    [-math.sin(-tcr_radians), 0, math.cos(-tcr_radians)]
+], dtype=np.float32)
 
+R = np.matmul(R_right, R_left.T)
+# R = R_right @ np.linalg.inv(R_left)
+
+# R = np.array([
+#     [math.cos(tcr_radians), 0, math.sin(tcr_radians)],
+#     [0, 1, 0],
+#     [-math.sin(tcr_radians), 0, math.cos(tcr_radians)]
+# ])
+
+baseline = 500  # Distance between the two cameras in mm
+toe_in_angle = baseline * math.tan(tcr_radians)
+
+T = np.array([
+    [baseline], [0], [toe_in_angle]
+], dtype=np.float32)
+
+# , dtype=np.float32
+
+# R_vec, _ = cv.Rodrigues(R)
+# print(left_matrix)
+# print(left_dist)
+# print(right_matrix)
+# print(right_dist)
+
+# print(R)
+# print(T)
+
+left_matrix = np.float32(left_matrix)
+left_dist = np.float32(left_dist)
+right_matrix = np.float32(right_matrix)
+right_dist = np.float32(right_dist)
+R = np.float32(R)
+T = np.float32(T)
+
+print(left_matrix)
+print(left_dist)
+print(right_matrix)
+print(right_dist)
+
+print(R)
+print(T)
+
+print("left_matrix:", left_matrix.dtype)
+print("left_dist:", left_dist.dtype)
+print("right_matrix:", right_matrix.dtype)
+print("right_dist:", right_dist.dtype)
+print("R:", R.dtype)
+print("T:", T.dtype)
+
+print("left_matrix shape:", left_matrix.shape)
+print("left_dist shape:", left_dist.shape)
+print("right_matrix shape:", right_matrix.shape)
+print("right_dist shape:", right_dist.shape)
+print("R shape:", R.shape)
+print("T shape:", T.shape)
+
+LR, RR, p1, p2, q = cv.stereoRectify(left_matrix, left_dist, right_matrix, right_dist, (1024, 1024), R, T)
 
 left_point = None
 right_point = None

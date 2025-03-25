62ddf959412a85f511d551fb6cb56ff0684b6a40
Henry Howe <heh44@aber.ac.uk>
2025-03-25 16:46:48 +0000

    creating code to extract camera data from txt file

---
+++ HenryFiles/Code/2025_03_25-reading camera data from txt.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+# Read the entire file and split into lines
+with open('camera_data.txt', 'r') as file:
+    lines = file.readlines()
+
+# Remove any empty lines and strip newline characters
+lines = [line.strip() for line in lines if line.strip() != '']
+
+# Parse Left Camera Matrix (Lines 0 to 2)
+left_camera_matrix = [list(map(float, lines[i].split())) for i in range(3)]
+left_camera_matrix = np.array(left_camera_matrix)
+
+# Parse Left Camera Distortion Coefficients (Line 3)
+left_camera_dist = list(map(float, lines[3].split()))
+left_camera_dist = np.array(left_camera_dist).reshape(-1, 1)  # Reshape
+
+# Parse Right Camera Matrix (Lines 4 to 6)
+right_camera_matrix = [list(map(float, lines[i].split())) for i in range(4, 7)]
+right_camera_matrix = np.array(right_camera_matrix)
+
+# Parse Right Camera Distortion Coefficients (Line 7)
+right_camera_dist = list(map(float, lines[7].split()))
+right_camera_dist = np.array(right_camera_dist).reshape(-1, 1)  # Reshape
+
+# Output the results
+print("Left Camera Matrix:")
+print(left_camera_matrix)
+print("\nLeft Camera Distortion Coefficients:")
+print(left_camera_dist)
+print("\nRight Camera Matrix:")
+print(right_camera_matrix)
+print("\nRight Camera Distortion Coefficients:")
+print(right_camera_dist)
+
+file.close()

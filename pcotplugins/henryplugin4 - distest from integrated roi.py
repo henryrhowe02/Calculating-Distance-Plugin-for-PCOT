a45d6439e873b3bf9c905a1ce875121f50be7ac6
Henry Howe <heh44@aber.ac.uk>
2025-04-25 14:26:47 +0100

    updated plugin to work with multidot to select multiple points and label them

--- pcotplugins/henryplugin4 - distest from integrated roi.py
+++ pcotplugins/henryplugin4 - distest from integrated roi.py
@@ -1,9 +1,10 @@
 import cv2 as cv
 import numpy as np
+from pcot.ui.canvas import Canvas
 from pcot.ui.tabs import Tab
 from pcot.value import Value
 from pcot.sources import nullSourceSet
-from PySide2.QtWidgets import QGridLayout, QLabel, QVBoxLayout, QTableWidget, QTableWidgetItem
+from PySide2.QtWidgets import QGridLayout, QLabel, QVBoxLayout, QTableWidget, QTableWidgetItem, QHBoxLayout
 
 from pcot.parameters.taggedaggregates import TaggedDictType
 from pcot.rois import ROICircle, ROIPainted, ROIPoly, ROIRect
@@ -27,11 +28,9 @@ class XFormDistEstimateRoi(XFormType):
     """
     A node designed to take the points selected in two rectified images 
     and then calculate the distance to said point
-    This version should take the integrated ROIs which are stored within the 
-    IMG Datum.
-    The code also outputs the distance to the tab, alongside the calculation for 
-    the distance "as-the-crow-flies" by using the height of the camera and some 
-    basic pythagoras.
+    This version takes in multiple rois and then returns 
+    the distance for all of them.
+    The distances are all displayed in a table
 
     Author: Henry Howe
     Date:2025-04-22
@@ -44,6 +43,8 @@ class XFormDistEstimateRoi(XFormType):
         # HARDCODED DATA
         self.focal_length = 2172.176065052148
         self.baseline = 0.5
+
+        self.all_distances = []
 
         self.addInputConnector("left", Datum.IMG)
         self.addInputConnector("right", Datum.IMG)
@@ -65,6 +66,9 @@ class XFormDistEstimateRoi(XFormType):
         pass
 
     def perform(self, node):
+        
+        self.all_distances = []
+
         left_img_datum = node.getInput(0)  
         right_img_datum = node.getInput(1)  
 
@@ -92,7 +96,11 @@ class XFormDistEstimateRoi(XFormType):
         left_rois_sorted = self.extract_and_check_rois(left_img_datum)
         right_rois_sorted = self.extract_and_check_rois(right_img_datum)
 
-        all_distances = []
+        print("Sorted ROIs")
+        print(left_rois_sorted)
+        print(right_rois_sorted)
+
+        # all_distances = []
 
         for label in sorted(left_rois_sorted.keys() & right_rois_sorted.keys()):
             print("Processing label:", label)
@@ -118,7 +126,7 @@ class XFormDistEstimateRoi(XFormType):
             right_roi = right_rois_match[0]
             storage = self.store_distance_and_rois(distance, left_roi, right_roi)
 
-            all_distances.append(storage)
+            self.all_distances.append(storage)
             # left_rois_match = left_rois_sorted[label]
             # right_rois_match = right_rois_sorted[label]
 
@@ -136,12 +144,15 @@ class XFormDistEstimateRoi(XFormType):
 
             # all_distances.append(storage)
 
-        node.all_distances = all_distances
+        node.all_distances = self.all_distances
+        node.left_rectified = left_img_datum.get(Datum.IMG)
+        node.right_rectified = right_img_datum.get(Datum.IMG)
 
-        print("Computed distances:", all_distances)
+        print("Computed distances:", self.all_distances)
+        
 
-        if all_distances:
-            node.setOutput(0, Datum(Datum.NUMBER, Value(all_distances[0]['distance']), nullSourceSet))
+        if self.all_distances:
+            node.setOutput(0, Datum(Datum.NUMBER, Value(self.all_distances[0]['distance']), nullSourceSet))
         else:
             node.setOutput(0, Datum(Datum.NUMBER, Value(float('nan')), nullSourceSet))
 
@@ -189,8 +200,11 @@ class XFormDistEstimateRoi(XFormType):
         # if node.tab is not None:
         #     node.tab.update()
 
-    def getDistanceList(self):
-        return getattr(self, 'all_distances', [])
+    # def getDistanceList(self):
+    #     print("Getting distance list")
+    #     all_distances = getattr(self, 'all_distances', [])
+    #     print(f"Returning {len(distances)} distances")
+    #     return distances
 
     def extract_roi_points(self, roi):
         if isinstance(roi, ROIPoly):
@@ -234,6 +248,7 @@ class XFormDistEstimateRoi(XFormType):
         Raises:
         UnlabeledROIException: If multiple ROIs are present and any ROI is unlabeled.
         """
+        print(f"Checking datum of type {datum.tp} for ROIs")
         if datum.tp in (Datum.ROI, Datum.IMG, Datum.VARIANT, Datum.ANY):
             rois = None
             if datum.tp == Datum.IMG:
@@ -247,8 +262,10 @@ class XFormDistEstimateRoi(XFormType):
                     rois = datum.val
 
             if rois:
+                print(f"Found {len(rois)} ROIs")
                 roi_dict = {}
                 for roi in rois:
+                    print(f"Checking ROI {roi.label if roi.label else 'with no label'}")
                     if not roi.label:
                         if len(rois) > 1:
                             raise UnlabeledROIException("Multiple ROIs must be labeled.")
@@ -259,8 +276,10 @@ class XFormDistEstimateRoi(XFormType):
                 
                 # Sorting ROIs by label
                 sorted_rois = {label: roi_dict[label] for label in sorted(roi_dict)}
+                print(f"Returning {len(sorted_rois)} labeled ROIs")
                 return sorted_rois
 
+        print("No ROIs found")
         return {}
 
     def store_distance_and_rois(self, distance, left_roi, right_roi):
@@ -292,26 +311,67 @@ class UnlabeledROIException(Exception):
 class TabDistEstimateRoi(Tab):
     def __init__(self, node, w):
         super().__init__(w, node)
-        self.layout = QVBoxLayout(self.w)
+
+        # self.layout = QVBoxLayout(self.w)
+        self.layout = QHBoxLayout(self.w)
+        self.canvas_layout = QVBoxLayout(self.w)
+
+        self.left_canvas = Canvas(self)
+        self.right_canvas = Canvas(self)
+
+        self.left_canvas.setGraph(node.graph)
+        self.right_canvas.setGraph(node.graph)
+
+        self.canvas_layout.addWidget(self.left_canvas)
+        self.canvas_layout.addWidget(self.right_canvas)
+
+        self.layout.addLayout(self.canvas_layout)
         
         self.table = QTableWidget()
         self.table.setColumnCount(3)
-        self.table.setHorizontalHeaderLabels(["Left Img ROI Label", "Right Img ROI Label", "Distance"])
+        self.table.setHorizontalHeaderLabels(["Left ROI Label", "Right ROI Label", "Distance"])
+    
+        self.table.setColumnWidth(0, 200)
+        self.table.setColumnWidth(1, 200)
+        self.table.setColumnWidth(2, 250)        
+        
         self.layout.addWidget(self.table)
         
         self.nodeChanged()
 
     def onNodeChanged(self):
         node = self.node
-        distance_list = node.type.getDistanceList()  # Assume this method returns the all_distances list
+        distance_list = node.all_distances
+        # distance_list = node.type.getDistanceList()  # Assume this method returns the all_distances list
+        print(distance_list)
         self.populate_table(distance_list)
 
+        if hasattr(node, 'left_rectified') and node.left_rectified is not None:
+            print("Left rectified image is available")
+            # left_img_cube = ImageCube(node.left_rectified)
+            left_img_cube = node.left_rectified
+
+            self.left_canvas.display(left_img_cube)
+        else:
+            print("Left rectified image is not available")
+
+        if hasattr(node, 'right_rectified') and node.right_rectified is not None:
+            print("Right rectified image is available")
+            # right_img_cube = ImageCube(node.right_rectified)
+            right_img_cube = node.right_rectified
+            self.right_canvas.display(right_img_cube)
+        else:
+            print("Right rectified image is not available")
+
     def populate_table(self, distance_list):
+        print(f"Populating table with {len(distance_list)} entries")
         self.table.setRowCount(len(distance_list))
         for row_index, data in enumerate(distance_list):
             left_label = data['left_roi']['label']
             right_label = data['right_roi']['label']
             distance = data['distance']
+
+            print(f"Row {row_index}: Left label: {left_label}, Right label: {right_label}, Distance: {distance}")
 
             self.table.setItem(row_index, 0, QTableWidgetItem(left_label))
             self.table.setItem(row_index, 1, QTableWidgetItem(right_label))

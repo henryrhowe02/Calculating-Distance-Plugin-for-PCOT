d8b05e179067bc4e3d26a8ed650ce0e248b76eaa
Henry Howe <heh44@aber.ac.uk>
2025-04-30 16:39:26 +0100

    code now creates side_length step by step from the known data, the size of the pixel. Also code, doesnt overwrite entire json file, just the focal_length, allowing baseline and height to be edited just from the json file

--- pcotplugins/pcotdistanceestimate plugins/focal_length_calculator.py
+++ pcotplugins/pcotdistanceestimate plugins/focal_length_calculator.py
@@ -1,10 +1,31 @@
 import json
 import os
+import numpy as np
+
+# === ORIGINALLY HARDCODED ===
+# side_length = 7.0656 # in mm
+# ============================
 
 file_path = 'pcotplugins/pcotdistanceestimate plugins/focal_baseline_height.json'
 
 baseline = 0.5 # in meters
-side_length = 7.0656 # in mm
+
+aupe_pixel_size = 3.45 # in um
+
+# Bin Pixels
+binned_pixel_size = aupe_pixel_size * 2 # 6.9um
+
+# Size of image = 1024 x 1024
+
+effective_focal_pane_side_um = 1024 * binned_pixel_size # 7065.6um
+print(effective_focal_pane_side_um)
+
+effective_focal_pane_side_mm = effective_focal_pane_side_um / 1000 # 7.0656mm
+print(effective_focal_pane_side_mm)
+
+side_length = round(effective_focal_pane_side_mm, 5 - int(np.floor(np.log10(abs(effective_focal_pane_side_mm)))) - 1)
+print(side_length)
+
 focal_length_mm = 12.1 # in mm
 camera_height = 1.094
 
@@ -15,12 +36,19 @@ def focal_length_from_sensor_width(sensor_width_mm, focal_length_mm, image_width
     return (focal_length_mm / sensor_width_mm) * image_width_pixels
 
 def generate_data():
-    with open('focal_baseline_height.json', 'w') as f:
-        json.dump({
-            'focal_length': focal_length_from_sensor_width(side_length, focal_length_mm, 1024),
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+    else:
+        data = {
             'baseline': baseline,
             'camera_height': camera_height
-        }, f, indent=4)
+        }
+    
+    data['focal_length'] = focal_length_from_sensor_width(side_length, focal_length_mm, 1024)
+    
+    with open(file_path, 'w') as f:
+        json.dump(data, f, indent=4)
 
 # try:
 #     with open('focal_baseline_height.json') as f:
